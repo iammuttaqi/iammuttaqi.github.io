2d07fb71f2b29c75a4877aa311f869ed66aba7ed
iammuttaqi <muntasermuttaqi@gmail.com>
2024-10-23 18:08:46 +0600

    nuxt ui build directory change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,19 @@ export default defineNuxtConfig({
   },
   colorMode: {
     preference: 'light'
+  },
+  buildDir: 'docs',
+  app: {
+    head: {
+      title: 'Muntaser Muttaqi',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
   }
 })
