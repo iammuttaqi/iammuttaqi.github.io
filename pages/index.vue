04b28462a1c047f208855237385cb5d37ee38618
iammuttaqi <muntasermuttaqi@gmail.com>
2024-10-24 07:21:00 +0600

    use of nuxt ui

--- pages/index.vue
+++ pages/index.vue
@@ -13,54 +13,45 @@ data.value = response
         </div>
 
         <div class="divide-y divide-gray-400">
-          <div class="space-y-5 pb-10 text-white">
-            <h3 class="gap-1 font-bold uppercase flex items-center">
-              <UIcon name="i-mdi-user" class="size-6" />
-              <span>About Me</span>
-            </h3>
-            <p>Full-stack Developer with over three years of experience in building modern web applications.
-            </p>
+          <div class="space-y-4 pb-8 text-white">
+            <UButton color="white" class="uppercase p-0 hover:no-underline" variant="link"
+              icon="i-heroicons-user-circle">About Me
+            </UButton>
+            <p class="text-sm">Full-stack Developer with over three years of experience in building modern web
+              applications.</p>
           </div>
 
-          <div class="space-y-5 py-10 text-white">
-            <h3 class="gap-1 font-bold uppercase flex items-center">
-              <UIcon name="i-mdi-phone" class="size-6" />
-              <span>Contact Info</span>
-            </h3>
+          <div class="space-y-4 py-8 text-white">
+            <UButton color="white" class="uppercase p-0 hover:no-underline" variant="link" icon="i-heroicons-phone">
+              Contact Info</UButton>
+
             <div class="space-y-2 text-sm">
               <div class="gap-1 flex items-center" v-for="contact in data.contacts">
-                <UIcon :name="contact.icon" class="size-5" />
-                <a class="transition-all hover:underline" :href="contact.href" target="_blank">{{
-                  contact.value }}</a>
+                <UButton color="white" class="p-0 text-sm" variant="link" :icon="contact.icon" :to="contact.href"
+                  target="_blank">{{ contact.value }}</UButton>
               </div>
             </div>
           </div>
 
-          <div class="space-y-5 py-10 text-white">
-            <h3 class="flex gap-1 font-bold uppercase items-center">
-              <UIcon name="i-mdi-link" class="size-6" />
-              <span>Links</span>
-            </h3>
+          <div class="space-y-4 py-8 text-white">
+            <UButton color="white" class="uppercase p-0 hover:no-underline" variant="link" icon="i-heroicons-link">Links
+            </UButton>
+
             <div class="space-y-2 text-sm">
               <div class="flex gap-1 items-center" v-for="link in data.links">
-                <UIcon :name="link.icon" class="size-5" />
-                <a class="transition-all hover:underline" :href="link.value" target="_blank">{{
-                  link.value }}</a>
+                <UButton color="white" class="p-0 text-sm" variant="link" :icon="link.icon" :to="link.value"
+                  target="_blank">{{ link.value }}</UButton>
               </div>
             </div>
           </div>
 
-          <div class="space-y-5 pt-10 text-white">
-            <h3 class="flex gap-1 font-bold uppercase items-center">
-              <UIcon name="i-mdi-translate" class="size-6" />
-              <span>Languages</span>
-            </h3>
+          <div class="space-y-4 pt-8 text-white">
+            <UButton color="white" class="uppercase p-0 hover:no-underline" variant="link" icon="i-heroicons-language">
+              Languages</UButton>
             <div class="space-y-4 text-sm">
-              <div class="space-y-1" v-for="language in data.languages">
+              <div class="space-y-2" v-for="language in data.languages">
                 <p>{{ language.title }}</p>
-                <div class="bg-gray-500">
-                  <div class="h-2 bg-white" :class="language.value"></div>
-                </div>
+                <UProgress :value="language.value" color="green" />
               </div>
             </div>
           </div>
@@ -74,23 +65,20 @@ data.value = response
           <div class="flex flex-col items-start justify-between gap-3 md:flex-row md:items-center md:gap-0">
             <div class="space-y-2 md:space-y-0">
               <h1 class="text-3xl font-semibold md:text-5xl">{{ data.name }}</h1>
-              <h4 class="text-xs md:text-base"><strong>3+ years</strong> of experience | <strong>Web
-                  Development</strong></h4>
+              <h4 class="text-xs md:text-base">{{ data.summary }}</h4>
             </div>
-            <a class="rounded-md border border-dashed border-gray-500 px-2 py-1 text-sm transition-all hover:bg-gray-100 flex gap-1 items-center"
-              download href="/muntaser-muttaq-resume.pdf" target="_blank">
-              <UIcon name="i-heroicons-arrow-down-tray" class="size-4" />Download
-            </a>
+            <UButton icon="i-heroicons-arrow-down-tray" size="2xs" color="gray"
+              class="border border-dashed border-gray-500 hover:bg-gray-100 transition-all hover:no-underline"
+              variant="link" label="Download" :trailing="false" download to="/muntaser-muttaq-resume.pdf"
+              target="_blank" />
           </div>
         </div>
 
         <!-- Work Experiences -->
         <div class="space-y-5 text-sm">
           <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4">
-            <h3 class="-ml-3 space-x-2 text-xl md:col-start-2 flex items-center">
-              <UIcon name="i-heroicons-briefcase" class="size-6" />
-              <span>Work Experiences</span>
-            </h3>
+            <UButton icon="i-heroicons-briefcase" size="xl" color="black" variant="link" label="Work Experiences"
+              :trailing="false" class="flex items-center -ml-3 md:col-start-2 hover:no-underline p-0" />
           </div>
 
           <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4"
@@ -109,14 +97,12 @@ data.value = response
                           class="text-gray-600 transition-all hover:underline" :href="experience.link"
                           target="_blank">{{ experience.link }}</a>)</span>
                     </p>
-                    <p class="text-xs text-gray-600 flex items-center">
-                      <UIcon name="i-heroicons-map-pin" class="size-4" />
-                      <span>Delaware City, USA</span>
-                    </p>
+                    <UButton icon="i-heroicons-map-pin" size="2xs" color="black" variant="link"
+                      :label="experience.location" :trailing="false" class="p-0" v-if="experience.location" />
                   </div>
                 </div>
                 <div class="text-gray-600">
-                  <h5 class="font-bold">Responsibilities : </h5>
+                  <h5 class="font-bold">Responsibilities: </h5>
                   <ul class="list-disc px-5 py-2 space-y-2">
                     <li v-for="responsibility in experience.responsibilities">{{ responsibility }}</li>
                   </ul>
@@ -129,10 +115,8 @@ data.value = response
         <!-- Skills -->
         <div class="space-y-5 text-sm">
           <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4">
-            <h3 class="-ml-3 space-x-2 text-xl md:col-start-2 flex items-center">
-              <UIcon name="i-heroicons-folder-open" class="size-6" />
-              <span>Skills</span>
-            </h3>
+            <UButton icon="i-heroicons-folder-open" size="xl" color="black" variant="link" label="Skills"
+              :trailing="false" class="flex items-center -ml-3 md:col-start-2 hover:no-underline p-0" />
           </div>
 
           <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4">
@@ -158,55 +142,26 @@ data.value = response
         <!-- Education -->
         <div class="space-y-5 text-sm">
           <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4">
-            <h3 class="-ml-3 space-x-2 text-xl md:col-start-2 flex items-center">
-              <UIcon name="i-heroicons-academic-cap" class="size-6" />
-              <span>Education</span>
-            </h3>
+            <UButton icon="i-heroicons-academic-cap" size="xl" color="black" variant="link" label="Education"
+              :trailing="false" class="flex items-center -ml-3 md:col-start-2 hover:no-underline p-0" />
           </div>
 
           <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4" v-for="education in data.educations">
             <div class="col-span-1 pb-5 md:pb-0">{{ education.timeline }}</div>
             <div class="relative col-span-3 pl-5 md:pl-10">
-              <div class="absolute -left-1.5 h-3 w-3 rounded-full border border-white bg-gray-700">
-              </div>
+              <div class="absolute -left-1.5 h-3 w-3 rounded-full border border-white bg-gray-700"></div>
               <div class="space-y-3">
                 <div class="space-y-2">
                   <h4 class="text-lg font-bold">{{ education.degree }}</h4>
                   <div class="flex flex-col justify-between gap-2 md:flex-row md:gap-0">
-                    <p>
-                      <span class="font-bold text-blue-800">{{ education.institution }}</span>
-                    </p>
-                    <p class="text-xs text-gray-600 flex items-center">
-                      <UIcon name="i-heroicons-map-pin" class="size-4" />
-                      <span>{{ education.location }}</span>
-                    </p>
+                    <p class="font-bold text-blue-800">{{ education.institution }}</p>
+                    <UButton icon="i-heroicons-map-pin" size="2xs" color="black" variant="link"
+                      :label="education.location" :trailing="false" class="p-0" v-if="education.location" />
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <!-- <div class="grid grid-cols-1 divide-x-2 divide-gray-300 md:grid-cols-4">
-            <div class="col-span-1 pb-5 md:pb-0">August 2014 - January 2019</div>
-            <div class="relative col-span-3 pl-5 md:pl-10">
-              <div class="absolute -left-1.5 h-3 w-3 rounded-full border border-white bg-gray-700">
-              </div>
-              <div class="space-y-3">
-                <div class="space-y-2">
-                  <h4 class="text-lg font-bold">Diploma in Computer Science and Technology</h4>
-                  <div class="flex flex-col justify-between gap-2 md:flex-row md:gap-0">
-                    <p>
-                      <span class="font-bold text-blue-800">Feni Computer Institute</span>
-                    </p>
-                    <p class="text-xs text-gray-600 flex items-center">
-                      <UIcon name="i-heroicons-map-pin" class="size-4" />
-                      <span>Feni, Bangladesh</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> -->
         </div>
       </div>
     </div>
